d092d2f78b1b90135b40ce9da7d1ca2e1d13f371
k0edb <jeanlamego@hotmail.com>
2022-11-21 19:45:54 -0300

    api consumida

---
+++ WeatherApp/js/main.js
@@ -0,0 +1,22 @@
+let weather={
+    "apiKey":"de44d6a961bec9dd4dce6385403fe381",
+    fetchWeather: function(city){
+        fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q="
+        + city
+        + "&units=metric&appid="
+        + this.apiKey
+        )
+        .then((response) =>response.json())
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data){
+        const {name}= data;
+        const {icon, description} = data.weather[0];
+        const {humidity, temp, feels_like} = data.main;
+        const {speed}= data.wind;
+        console.log(name,icon,description,humidity,temp,feels_like,speed)
+        document.querySelector(".city").innerText = "Tempo agora em " + name;
+        document.querySelector(".icon").src="https://openweathermap.org/img/wn/"+ icon +".png";
+    }
+};
--- WeatherApp/readme.md
+++ WeatherApp/readme.md
@@ -5,9 +5,9 @@
 - [x] Definir as melhores definições para Brasil
 - [x] Montar a estrutura do HTML
 - [x] Background Automático
-- []
 - [x] Criar a div do card
 - [x] Dar uma Checada na Responsividade
+- [] Dar uma entendida em JS
 - [] Criar o button com JS
 - [] --Aprender a consumir API--
 - [] Finalizar o Projeto
